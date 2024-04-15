8eaedb50444477cc72c5c8dab66ad07398217e78
Merna Gamal <merna.gamal.shenouda@gmail.com>
2024-04-15 20:07:13 +0200

    first commit

---
+++ Backend/server.js
@@ -0,0 +1,24 @@
+
+//#region Requires
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT||7005;
+const bodyParser = require("body-parser");
+const OrdersRoutes = require("./Routes/OrdersRoutes");
+const ProductsRoutes = require("./Routes/ProductsRoutes");
+const UsersRoutes = require("./Routes/UsersRoutes");
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/E-Commerce") //Orders Users Products
+//#endregion
+
+//#region MiddleWare
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+//#endregion
+
+app.use("/api/orders", OrdersRoutes);
+app.use("/api/products", ProductsRoutes);
+app.use("/api/users", UsersRoutes)
+
+app.listen(PORT, ()=>{console.log("http://localhost:"+PORT)})
+
