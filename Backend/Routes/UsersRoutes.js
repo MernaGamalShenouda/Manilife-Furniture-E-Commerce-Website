e3b55af993c9e9562dcec6ccdc3b905ae18e5a09
Nardine William <nardine.william@gmail.com>
2024-04-20 19:50:01 +0200

    feat: Added Controllers and Routes to read all users / read user by id / delete user by id

--- Backend/Routes/UsersRoutes.js
+++ Backend/Routes/UsersRoutes.js
@@ -1,9 +1,12 @@
-const express = require("express")
+const express = require("express");
 const router = new express.Router();
 const UsersController = require("../Controllers/UsersControllers");
 
 router.post("/signup", UsersController.Register);
 router.post("/login", UsersController.Login);
-router.put("/:id",UsersController.updateUserById)
+router.put("/:id", UsersController.updateUserById);
+router.get("/", UsersController.getAllUsers);
+router.get("/:id", UsersController.getUserById);
+router.delete("/:id", UsersController.deleteUserById);
 
 module.exports = router;
