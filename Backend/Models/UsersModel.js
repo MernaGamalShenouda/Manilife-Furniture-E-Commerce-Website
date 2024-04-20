73d438a47dfd69f13bee443aa55f6b551a4fdbac
Nardine William <nardine.william@gmail.com>
2024-04-20 22:47:10 +0200

    feat: Added role to users

--- Backend/Models/UsersModel.js
+++ Backend/Models/UsersModel.js
@@ -9,18 +9,23 @@ const mongoose = require("mongoose");
 // });
 
 const cartItemSchema = new mongoose.Schema({
-    productId: String, 
-    quantity: Number,
+  productId: String,
+  quantity: Number,
 });
 
 const usersSchema = new mongoose.Schema({
-    username: String,
-    fullname: String,
-    email: String,
-    password: String,
-    image: String,
-    gender: String,
-    cart: [cartItemSchema] 
+  username: String,
+  fullname: String,
+  email: String,
+  password: String,
+  image: String,
+  gender: String,
+  cart: [cartItemSchema],
+  role: {
+    type: String,
+    enum: ["user", "admin"],
+    default: "user",
+  },
 });
 
-module.exports =  mongoose.model("users",usersSchema)
+module.exports = mongoose.model("users", usersSchema);
