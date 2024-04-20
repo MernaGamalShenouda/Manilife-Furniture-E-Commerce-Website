d8d172d2b7e860629db56e691546af3afd30f6f3
Nardine William <nardine.william@gmail.com>
2024-04-20 23:16:44 +0200

    fix: uncomment some lines

--- Backend/server.js
+++ Backend/server.js
@@ -3,9 +3,9 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 7005;
 const bodyParser = require("body-parser");
-// const OrdersRoutes = require("./Routes/OrdersRoutes");
-// // const ProductsRoutes = require("./Routes/ProductsRoutes");
-// const UsersRoutes = require("./Routes/UsersRoutes");
+const OrdersRoutes = require("./Routes/OrdersRoutes");
+const ProductsRoutes = require("./Routes/ProductsRoutes");
+const UsersRoutes = require("./Routes/UsersRoutes");
 const mongoose = require("mongoose");
 // mongoose.connect("mongodb://localhost:27017/E-Commerce") //Orders Users Products
 //#endregion
@@ -31,8 +31,8 @@ app.use(bodyParser.json());
 // app.set('view engine', 'view');
 //#endregion
 
-// app.use("/api/orders", OrdersRoutes);
-// app.use("/api/products", ProductsRoutes);
+app.use("/api/orders", OrdersRoutes);
+app.use("/api/products", ProductsRoutes);
 app.use("/api/users", UsersRoutes);
 
 app.listen(PORT, () => {
