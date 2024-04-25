853bf7649fe9de9481ec01cf818d9c9fd0e7218f
Mohamed Torkey <120189395+mohamedtorkey20@users.noreply.github.com>
2024-04-25 02:06:46 +0200

    added version two from dashboard

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -2,12 +2,12 @@ import { Routes } from '@angular/router';
 import { ProductsComponent } from './components/admin/products/products.component';
 import { OrdersComponent } from './components/admin/orders/orders.component';
 import { CreateProductComponent } from './components/admin/create-product/create-product.component';
+import { DashboardComponent } from './components/admin/dashboard/dashboard.component';
 
 export const routes: Routes = [
-  {path:"",component:ProductsComponent},
-  {path:"Products",component:ProductsComponent},
-  {path:"CreateProduct",component:CreateProductComponent},
-
-  {path:"Orders",component:OrdersComponent}
+  {path:"dashboard",component:DashboardComponent},
+  {path:"adminProducts",component:ProductsComponent},
+  {path:"adminCreateProduct",component:CreateProductComponent},
+  {path:"adminOrders",component:OrdersComponent}
 
 ];
--- Frontend/src/app/components/admin/orders/orders.component.ts
+++ Frontend/src/app/components/admin/orders/orders.component.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { AdminServiceService } from '../admin-service.service';
+import { HttpClientModule } from '@angular/common/http';
+import {MatChipsModule} from '@angular/material/chips';
 @Component({
   selector: 'app-orders',
   standalone: true,
-  imports: [],
+  imports: [
+    HttpClientModule,
+    MatChipsModule
+  ],
+  providers:[AdminServiceService],
   templateUrl: './orders.component.html',
   styleUrl: './orders.component.css'
 })
-export class OrdersComponent {
+export class OrdersComponent  implements OnInit {
+
+  Orders:any[]=[];
+
+  constructor(private adminService:AdminServiceService){}
+
+
+  ngOnInit(): void {
+    this.GetOrders()
+  }
+
+
+
+//------------Get All Orders----------------------------------
+  GetOrders():void{
+    this.adminService.GetOrders().subscribe({
+      next:(data:any)=>{
+        this.Orders=data;
+     
+
+      },
+
+      error:error=>{
+        console.error(error);
+
+      }
+    })
+  }
+
+
+
 
 }
