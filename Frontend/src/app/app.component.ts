3d174261360262f43ce509b843c50799ffd1638c
Nardine William <nardine.william@gmail.com>
2024-04-26 01:33:32 +0300

    feat: Added Navigation component and Home Component

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { IndexComponent } from './components/admin/index/index.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet
-    ,IndexComponent
-  ],
+  imports: [RouterOutlet, IndexComponent, NavigationComponent, HomeComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'Frontend';
