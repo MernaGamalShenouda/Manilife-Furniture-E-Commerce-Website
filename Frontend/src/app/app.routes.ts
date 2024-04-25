780b6178deceb9bd96731732b0c44341e3d9c03f
ingy <ingy.mahmoud.elsakhawy@gmail.com>
2024-04-25 07:11:26 +0200

    Logic {display all items, search through items, search by category, display Product details }

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -2,12 +2,19 @@ import { Routes } from '@angular/router';
 import { ProductsComponent } from './components/admin/products/products.component';
 import { OrdersComponent } from './components/admin/orders/orders.component';
 import { CreateProductComponent } from './components/admin/create-product/create-product.component';
+import { ShopComponent } from './components/shop/shop.component';
+import { AllProductsComponent } from './components/all-products/all-products.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
 
 export const routes: Routes = [
-  {path:"",component:ProductsComponent},
-  {path:"Products",component:ProductsComponent},
-  {path:"CreateProduct",component:CreateProductComponent},
-
+  // {path:"",component:ProductsComponent},
+  // {path:"Products",component:ProductsComponent},
+  // {path:"CreateProduct",component:CreateProductComponent},
+  
+  { path: 'shop', component: ShopComponent, children: [
+    { path: "products", component: AllProductsComponent },
+    { path: "products/:id", component: ProductDetailsComponent }]},
+  
   {path:"Orders",component:OrdersComponent}
 
 ];
