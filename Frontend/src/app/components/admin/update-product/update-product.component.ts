bbf4efd9e10bb18ab07e06d8a94db35e9637a8df
Mohamed Torkey <120189395+mohamedtorkey20@users.noreply.github.com>
2024-04-25 14:45:23 +0200

    add some chanages

--- Frontend/src/app/components/admin/update-product/update-product.component.ts
+++ Frontend/src/app/components/admin/update-product/update-product.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
 import {MatDialogModule} from '@angular/material/dialog';
 import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
 
 @Component({
   selector: 'app-update-product',
   standalone: true,
-  imports: [MatDialogModule, MatButtonModule],
+  imports: [MatDialogModule,
+     MatButtonModule,
+     MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+  ],
   templateUrl: './update-product.component.html',
   styleUrl: './update-product.component.css'
 })
