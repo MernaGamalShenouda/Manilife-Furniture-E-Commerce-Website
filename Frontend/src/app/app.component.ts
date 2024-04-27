5d745eff0ba9c8786f2a26a95995511b59a76f96
Nardine William <nardine.william@gmail.com>
2024-04-27 04:53:28 +0300

    feat: Added routes and footer component

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -3,11 +3,20 @@ import { RouterOutlet } from '@angular/router';
 import { IndexComponent } from './components/admin/index/index.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, IndexComponent, NavigationComponent, HomeComponent],
+  imports: [
+    RouterOutlet,
+    IndexComponent,
+    NavigationComponent,
+    HomeComponent,
+    FooterComponent,
+    AboutComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -2,12 +2,17 @@ import { Routes } from '@angular/router';
 import { ProductsComponent } from './components/admin/products/products.component';
 import { OrdersComponent } from './components/admin/orders/orders.component';
 import { CreateProductComponent } from './components/admin/create-product/create-product.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 export const routes: Routes = [
-  {path:"",component:ProductsComponent},
-  {path:"Products",component:ProductsComponent},
-  {path:"CreateProduct",component:CreateProductComponent},
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
 
-  {path:"Orders",component:OrdersComponent}
+  // { path: '', component: ProductsComponent },
+  { path: 'Products', component: ProductsComponent },
+  { path: 'CreateProduct', component: CreateProductComponent },
 
+  { path: 'Orders', component: OrdersComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
