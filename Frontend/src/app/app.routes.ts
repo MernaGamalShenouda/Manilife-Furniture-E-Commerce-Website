f1b2c8037d69d740ebeec2c91e2fd13c51ea6d47
FatmaElzahraa227 <70411867+FatmaElzahraa227@users.noreply.github.com>
2024-04-27 04:08:11 +0300

    ..

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -2,11 +2,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProductsComponent } from './components/admin/products/products.component';
 import { OrdersComponent } from './components/admin/orders/orders.component';
 import { CreateProductComponent } from './components/admin/create-product/create-product.component';
-import { ShopComponent } from './components/shop/shop.component';
-import { AllProductsComponent } from './components/all-products/all-products.component';
-import { ProductDetailsComponent } from './components/product-details/product-details.component';
-import { RegisterComponent } from './components/users/register/register.component';
-import { LoginComponent } from './components/users/login/login.component';
 import { DashboardComponent } from './components/admin/dashboard/dashboard.component';
 import { AdminMainComponent } from './components/admin/admin-main/admin-main.component';
 import { AdminGuard } from './Guards/admin.guard';
@@ -15,8 +10,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { userAuthGuard } from './Guards/user-auth.guard';
 import { authGuard } from './Guards/auth.guard';
 import { adminAuthGuard } from './Guards/admin-auth.guard';
-import { ErrorComponent } from './components/error/error.component';
 import { HomeComponent } from './home/home.component';
+import { ErrorComponent } from './components/error/error.component';
+import { ShopComponent } from './components/shop/shop.component';
+import { AllProductsComponent } from './components/all-products/all-products.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
+import { RegisterComponent } from './components/users/register/register.component';
+import { LoginComponent } from './components/users/login/login.component';
 
 export const routes: Routes = [
   // {path:"",component:ProductsComponent},
