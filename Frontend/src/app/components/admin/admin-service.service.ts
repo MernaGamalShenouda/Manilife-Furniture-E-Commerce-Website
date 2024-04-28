cd67d507cc5b15eccf3ddb1c1593db86f397a891
Mohamed Torkey <120189395+mohamedtorkey20@users.noreply.github.com>
2024-04-28 13:32:19 +0300

    added upload image using cloudinary  for register page

--- Frontend/src/app/components/admin/admin-service.service.ts
+++ Frontend/src/app/components/admin/admin-service.service.ts
@@ -8,47 +8,47 @@ export class AdminServiceService {
 
   constructor(private readonly  http:HttpClient) { }
 
-  private URB_DB="http://localhost:7005/api/";
+  private URL_DB="http://localhost:7005/api/";
 
 
   GetProducts(page: number=0, pageSize: number=5) {
-    return this.http.get(this.URB_DB+`products?page=${page}&pageSize=${pageSize}`);
+    return this.http.get(this.URL_DB+`products?page=${page}&pageSize=${pageSize}`);
   }
 
   GetProductByName(title:string){
-    return this.http.get(this.URB_DB+"products/name/"+title);
+    return this.http.get(this.URL_DB+"products/name/"+title);
   }
 
 
   GetProductById(id:string){
-    return this.http.get(this.URB_DB+"products/"+id);
+    return this.http.get(this.URL_DB+"products/"+id);
   }
 
   GetOrders(){
-    return this.http.get(this.URB_DB+"orders");
+    return this.http.get(this.URL_DB+"orders");
   }
 
   creatProduct(product:any)
   {
-    return this.http.post(this.URB_DB +"products",product)
+    return this.http.post(this.URL_DB +"products",product)
   }
 
   updateProduct(product:any,productId:string){
-    return this.http.put(this.URB_DB+`products/${productId}`,product)
+    return this.http.put(this.URL_DB+`products/${productId}`,product)
   }
 
   deleteProduct(id:string)
   {
-    return this.http.delete(this.URB_DB+`products/${id}`)
+    return this.http.delete(this.URL_DB+`products/${id}`)
   }
 
   updateOrderState(id:any,state:any)
   {
-    return this.http.put(this.URB_DB+`orders/${id}`,{orderId:id,newState:state});
+    return this.http.put(this.URL_DB+`orders/${id}`,{orderId:id,newState:state});
   }
 
 
   uploadImage(formData: FormData) {
-    return this.http.post(this.URB_DB+`uploadPhoto/upload`, formData);
+    return this.http.post(this.URL_DB+`uploadPhoto/upload`, formData);
   }
 }
