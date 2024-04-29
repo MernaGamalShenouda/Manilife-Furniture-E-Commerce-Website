7e58b63dd7aefd7891a8bfe0d684f2e65d42061d
FatmaElzahraa227 <70411867+FatmaElzahraa227@users.noreply.github.com>
2024-04-29 18:43:34 +0300

    ..

--- Frontend/src/app/components/users/register/register.component.ts
+++ Frontend/src/app/components/users/register/register.component.ts
@@ -14,7 +14,7 @@ import { CommonModule } from '@angular/common';
   styleUrl: './register.component.css',
 })
 export class RegisterComponent {
-  url_Photo:any={}
+  url_Photo: any = {};
 
   @ViewChild('registerForm', { static: false }) registerForm!: NgForm;
 
@@ -23,43 +23,38 @@ export class RegisterComponent {
     fullname: '',
     email: '',
     password: '',
-    image:{},
-    gender: ''
+    image: {},
+    gender: '',
   };
 
-  emailError: string = ''; 
-  passError: string = ''; 
+  emailError: string = '';
+  passError: string = '';
   constructor(
     private registerService: RegisterService,
     private router: Router
   ) {}
 
   submitForm() {
-
-
-this.user.image=this.url_Photo.data.secure_url;
+    this.user.image = this.url_Photo.data.secure_url;
     this.registerForm.form.markAllAsTouched();
 
     if (this.registerForm.form.valid) {
-
       if (this.checkPasswordStrength(this.registerForm.controls['password'])) {
         console.log('Password is too weak. Please enter a stronger password.');
         return;
       }
 
-      this.registerService.AddUser(this.user).subscribe(
-        {
+      this.registerService.AddUser(this.user).subscribe({
         next: (response: any) => {
-            console.log('User added successfully:', response);
-            this.router.navigate(['/Login']);
-          },
-          error: (error) => {
-            console.error('Error adding user:', error);
-        }
-            this.emailError = error.error.text; 
-            console.log(this.emailError)
-        }
-      );
+          console.log('User added successfully:', response);
+          this.router.navigate(['/Login']);
+        },
+        error: (error) => {
+          console.error('Error adding user:', error);
+          this.emailError = error.error.text;
+          console.log(this.emailError);
+        },
+      });
     } else {
       console.log('Form is invalid. Please check your inputs.');
     }
@@ -67,29 +62,26 @@ this.user.image=this.url_Photo.data.secure_url;
 
   checkPasswordStrength(control: any) {
     if (control.value && control.value.length < 8) {
-      this.passError =  'Password is too weak. Please enter a stronger password.'; 
-      return { 'weakPassword': true };
+      this.passError =
+        'Password is too weak. Please enter a stronger password.';
+      return { weakPassword: true };
     }
     return null;
   }
 
+  //--------------Upload Photo----------------------------------
 
-
-//--------------Upload Photo----------------------------------
-
-uploadPhoto(event: any) {
-  const file: File = event.target.files[0];
-  const formData = new FormData();
+  uploadPhoto(event: any) {
+    const file: File = event.target.files[0];
+    const formData = new FormData();
     formData.append('image', file);
-  this.registerService.uploadImage(formData).subscribe({
-    next: data => {
-      this.url_Photo=data
-
-    },
-    error: err => {
-      console.error(err);
-    }
-  });
+    this.registerService.uploadImage(formData).subscribe({
+      next: (data) => {
+        this.url_Photo = data;
+      },
+      error: (err) => {
+        console.error(err);
+      },
+    });
+  }
 }
-}
-
