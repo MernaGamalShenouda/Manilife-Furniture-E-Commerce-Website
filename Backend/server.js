d06c27293a58d9aac87b923beacfeb2fc54dc495
Merna Gamal <merna.gamal.shenouda@gmail.com>
2024-05-01 20:58:25 +0300

    Deploy Fixes

--- Backend/server.js
+++ Backend/server.js
@@ -1,18 +1,18 @@
 //#region Requires
 const express = require("express");
-const cors = require("cors");
-require("dotenv").config();
+const cors = require('cors');
+require('dotenv').config();
 const app = express();
-const PORT = process.env.PORT || 7005;
+const PORT = process.env.PORT || 7007;
 const bodyParser = require("body-parser");
 const ProductsRoutes = require("./Routes/ProductsRoutes");
 const OrdersRoutes = require("./Routes/OrdersRoutes");
 const UsersRoutes = require("./Routes/UsersRoutes");
 const uploadRoute = require("./Routes/UploadRoutes");
 
-let Products = require("./Models/Products.Model");
+let Products  = require("./Models/Products.Model");
 
-const fs = require("fs");
+const fs = require('fs');
 
 app.use(cors());
 
@@ -20,7 +20,7 @@ const mongoose = require("mongoose");
 //#endregion
 
 //#region Connect to MongoDB
-mongoose.connect("mongodb://localhost:27017/E-Commerce", {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
@@ -32,43 +32,45 @@ db.on("error", console.error.bind(console, "MongoDB connection error:"));
 db.once("open", function () {
   console.log("Connected to MongoDB");
 
-  // Read data from JSON file
-  fs.readFile("data.json", "utf8", (err, data) => {
-    if (err) {
-      console.error("Error reading JSON file:", err);
-      return;
-    }
+   // Read data from JSON file
+  // fs.readFile('data.json', 'utf8', (err, data) => {
+  //   if (err) {
+  //     console.error('Error reading JSON file:', err);
+  //     return;
+  //   }
 
-    try {
-      const jsonData = JSON.parse(data);
+  //   try {
+  //     const jsonData = JSON.parse(data);
+  //     const jsonDataSlice = jsonData.slice(0, 20);
 
-      jsonData.forEach(async (item) => {
-        try {
-          const { name, price, category, short_description, images } = item;
+  //     jsonDataSlice.forEach(async (item) => {
+  //       try {
+  //         const { name, price, category, short_description, images } = item;
 
-          // Create a new product instance
-          const newProduct = new Products({
-            title: name,
-            price: price,
-            category: category,
-            quantity: 5,
-            images: images,
-            details: {
-              description: short_description,
-              reviews: [],
-            },
-          });
+  //         // Create a new product instance
+  //         const newProduct = new Products({
+  //           title: name,
+  //           price: price,
+  //           category: category,
+  //           quantity: 5, 
+  //           images: images, 
+  //           details: {
+  //             description: short_description,
+  //             reviews: [] 
+  //           }
+  //         });
 
-          await newProduct.save();
-          console.log("Product created:", newProduct);
-        } catch (err) {
-          console.error("Error creating product:", err);
-        }
-      });
-    } catch (parseError) {
-      console.error("Error parsing JSON data:", parseError);
-    }
-  });
+  //         await newProduct.save();
+  //         console.log('Product created:', newProduct);
+  //       } catch (err) {
+  //         console.error('Error creating product:', err);
+  //       }
+  //     });
+  //   } catch (parseError) {
+  //     console.error('Error parsing JSON data:', parseError);
+  //   }
+  // });
+  
 });
 //#endregion
 
@@ -81,8 +83,11 @@ app.use(bodyParser.json());
 app.use("/api/products", ProductsRoutes);
 app.use("/api/users", UsersRoutes);
 app.use("/api/orders", OrdersRoutes);
-app.use("/api/uploadPhoto", uploadRoute);
+app.use("/api/uploadPhoto" , uploadRoute);
+
 
 app.listen(PORT, () => {
   console.log("http://localhost:" + PORT);
 });
+
+module.exports = express;
